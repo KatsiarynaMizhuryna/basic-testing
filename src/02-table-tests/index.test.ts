2b8a8bf7b721be694f4871f1d6147eb6570a4ecd
Kate <ekaterina21051988@yandex.by>
2024-02-08 11:59:55 +0200

    feat: table-tests have been been created

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -9,20 +9,23 @@ const testCases = [
     { a: 4, b: 2, action: Action.Subtract, expected: 2 },
     { a: 7, b: 3, action: Action.Subtract, expected: 4 },
     { a: 4, b: 3, action: Action.Multiply,expected: 12 },
-    { a: 2, b: 3, action: Action.Multiply, expected: 4 },
+    { a: 2, b: 3, action: Action.Multiply, expected: 6 },
     { a: 3, b: 3, action: Action.Multiply, expected: 9 },
     { a: 6, b: 3, action: Action.Divide, expected: 2 },
     { a: 10, b: 2, action: Action.Divide, expected: 5},
-    { a: 9, b: 3, action: Action.Divide, expected: 9 },
-    { a: 'abc', b: 3, action: '+', expected: "null" },
-    { a: 2, b: 3, action: 'InvalidAction' as Action, expected: "null" },
-    { a: 2, b: 'abc', action: 'InvalidAction' as Action, expected: "null" }
+    { a: 9, b: 3, action: Action.Divide, expected: 3 },
+    { a: 4, b: 2, action: Action.Exponentiate, expected: 16},
+    { a: 3, b: 2, action: Action.Exponentiate, expected: 9},
+    { a: 1, b: 2, action: Action.Exponentiate, expected: 1},
+    { a: 'abc', b: 3, action: '+', expected: null },
+    { a: 2, b: 3, action: 'InvalidAction' as Action, expected: null},
+    { a: 2, b: 'abc', action: 'InvalidAction' as Action, expected: null }
 ]
 
 describe('simpleCalculator', () => {
   testCases.forEach(({ a, b, action, expected }) => {
     test(`should ${action} ${a} and ${b} to be ${expected}`, () => {
-      const result = simpleCalculator({ a, b, action });
+      const result: number|null = simpleCalculator({ a, b, action });
       expect(result).toBe(expected);
     });
   });
